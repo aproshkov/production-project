f541421064d19cbb3ea6d9be91573d55a1a0c80e
Aleksandr Proshkov <all9100@mail.ru>
2024-02-01 19:50:31 +0300

    add approuter,navbar components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,10 @@
 import './styles/index.scss'
-import { Route, Routes } from 'react-router-dom'
 import { Link } from 'react-router-dom'
-import { Suspense } from 'react'
 import { classNames } from 'shared/lib/classNames/classNames'
 import {useTheme} from 'app/providers/ThemeProvider'
 import './styles/index.scss';
-import { MainPage } from 'pages/MainPage'
-import { AboutPage } from 'pages/AboutPage'
+import { AppRouter } from './providers/router';
+import { Navbar } from 'widgets/Navbar/ui/Navbar';
 
 
 
@@ -17,15 +15,9 @@ export const App = () => {
 
     return (
         <div className={classNames('app', {}, [theme])}>
+        <Navbar/>
+        <AppRouter/>
         <button onClick={toggleTheme}>TOGGLE</button>
-        <Link to={'/'}> Главная</Link>
-        <Link to={'/about'}>О сайте</Link>
-        <Suspense fallback={<div>Loading..</div>}>
-            <Routes>
-                <Route path={'/'} element = {<MainPage/>} />
-                <Route path={'/about'} element = {<AboutPage/>} />
-            </Routes>
-        </Suspense>
     </div>
     )
 }
