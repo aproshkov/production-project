6e12180280feeee12566f98bfa55b38d3416092c
Aleksandr Proshkov <all9100@mail.ru>
2024-02-24 15:03:53 +0300

    добавил redux, первый тестовый entity

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,9 +1,13 @@
+import { Counter } from 'entities/Counter';
 import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
     const { t } = useTranslation('main');
     return (
-        <div>{t('Главная страница')}</div>
+        <>
+            <div>{t('Главная страница')}</div>
+            <Counter />
+        </>
     );
 };
 
