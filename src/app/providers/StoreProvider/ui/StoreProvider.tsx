10dc634191910e7181e6323646e7e149dc849aad
Aleksandr Proshkov <all9100@mail.ru>
2024-03-28 19:24:32 +0300

    фикс тестов сторибука

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -1,16 +1,17 @@
 import { FC, ReactNode } from 'react';
 import { Provider } from 'react-redux';
-import { DeepPartial } from '@reduxjs/toolkit';
+import { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
 import { createReduxStore } from '../config/store';
 import { StateSchema } from '../config/StateSchema';
 
 interface StoreProviderProps {
     children?: ReactNode;
     initialState?: DeepPartial<StateSchema>
+    asyncRedcurs?: DeepPartial<ReducersMapObject<StateSchema>>
 }
 
-export const StoreProvider: FC<StoreProviderProps> = ({ children, initialState }) => {
-    const store = createReduxStore(initialState as StateSchema);
+export const StoreProvider: FC<StoreProviderProps> = ({ children, initialState, asyncRedcurs }) => {
+    const store = createReduxStore(initialState as StateSchema, asyncRedcurs as ReducersMapObject<StateSchema>);
     return (
         <Provider store={store}>
             {children}
